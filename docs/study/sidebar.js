ec888c90578df5c389936a3708388deb5c4536e7
Chike <148038535+chike95@users.noreply.github.com>
2023-10-23 22:17:25 +0800

    arduino环境

--- docs/study/sidebar.js
+++ docs/study/sidebar.js
@@ -1,11 +1,42 @@
 module.exports = [
     {
-        title: '软硬结合-从零打造物联网',
-        path: '/study/1',
+        title: '研究学习',
         collapsable: true,
         sidebarDepth: 2,
-        // children: [
-        //     '/FE/VUE/base'
-        // ]
+        children: [
+            '/study/1'
+        ]
     },
 ]
+
+// module.exports = [
+
+//     {
+//         title: '硬件调试',
+//         path: '/FE/VUE/vuepress',
+//         collapsable: true,
+//         sidebarDepth: 2,
+//     },
+//     {
+//         title: '软件调试',
+//         path: '/FE/VUE/vuepress',
+//         collapsable: true,
+//         sidebarDepth: 2,
+//     },
+//     {
+//         title: '项目部署',
+//         path: '/work/deploy/',
+//         collapsable: true,
+//         sidebarDepth: 2,
+//         children: [
+//             '/work/deploy/windows_env',
+//             '/work/deploy/ubuntu_env',
+//         ]
+//     },
+//     {
+//         title: '项目记录',
+//         path: '/FE/VUE/vuepress',
+//         collapsable: true,
+//         sidebarDepth: 2,
+//     }
+// ]
