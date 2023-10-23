0bd73e5b91149b7513a8f5a974314d16d1e67b4b
chike95 <148038535+chike95@users.noreply.github.com>
2023-10-23 17:41:28 +0800

    es8266

---
+++ docs/EE/DB/sidebar.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        title: 'ESP8266',
+        path: '/EE/DB/esp8266',
+        collapsable: true,
+        sidebarDepth: 2,
+        // children: [
+        //     '/FE/VUE/base'
+        // ]
+    },
+    {
+        title: 'STM32F103',
+        path: '/EE/DB/stm32f103',
+        collapsable: true,
+        sidebarDepth: 2,
+    }
+]
